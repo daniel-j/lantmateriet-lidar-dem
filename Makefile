3004a51deb2807c253a8b1c7836fc90690d9cd3a
daniel-j <daniel.j.hede@gmail.com>
2022-06-02 19:27:21 +0200

    use lm folder structure, generate smaller tifs for previews

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 
-LIDARFILES = $(shell find input -maxdepth 1 -name '*.laz' | sort)
-DEMFILES = $(patsubst input/%.laz,output/dem_%.tif,$(LIDARFILES))
+INPUTDIR = input
+OUTPUTDIR = output
+LIDARFILES = $(shell find $(INPUTDIR) -maxdepth 3 -type f -name '*.laz' | sort)
+DEMFILES = $(patsubst $(INPUTDIR)/%.laz,$(OUTPUTDIR)/%.tif,$(LIDARFILES))
 
+.PHONY: server mount unmount
+.PRECIOUS: $(OUTPUTDIR)/%.tif
+default: $(DEMFILES) dem.vrt
 
-.PHONY: dem.vrt server
-.PRECIOUS: output/dem_%.tif
-default: dem.vrt
-
-output/dem_%.tif: input/%.laz
-	@mkdir -p output
-	time ./process.py "$<" "$@" 500
+$(OUTPUTDIR)/%.tif: $(INPUTDIR)/%.laz
+	@echo "Processing $<"
+	@mkdir -pv $(dir $@)
+	@time ./process.py "$<" "$@" 500
 #	@IFS='_' read -r -a arr <<< "$<"; time pdal pipeline ./pipeline_raster.json --readers.las.filename="$<" --writers.raster.filename="$@.tmp" --filters.faceraster.origin_y="$$(($${arr[1]}*100))" --filters.faceraster.origin_x="$$(($${arr[2]}*100))"
 #	@gdal_fillnodata.py -md 2500 "$@.tmp" "$@" -co COMPRESS=ZSTD
 #	@gdal_translate -srcwin 10 10 2500 2500 merged1f.tif merged1fc.tif -co COMPRESS=ZSTD
@@ -17,8 +19,15 @@ output/dem_%.tif: input/%.laz
 #	@rm -f "$@.tmp"
 	@echo "Finished writing $@"
 
-dem.vrt: $(DEMFILES)
-	@rm -f dem.vrt && gdalbuildvrt -overwrite dem.vrt output/dem_*.tif
+dem.vrt:
+	@rm -f dem.vrt && gdalbuildvrt -overwrite dem.vrt $(OUTPUTDIR)/*/*/*25.tif
 
 server: dem.vrt
 	uvicorn tiler:app --reload --host 0.0.0.0 --port 3610
+
+mount:
+	@gio mount ftp://download-opendata.lantmateriet.se < ftp.passwd
+	@ln -svf "/run/user/$$(id -u)/gvfs/ftp:host=download-opendata.lantmateriet.se/Laserdata_Skog" inputremote
+unmount:
+	@gio mount -u "/run/user/$$(id -u)/gvfs/ftp:host=download-opendata.lantmateriet.se"
+	@rm -f inputremote
--- process.py
+++ process.py
@@ -9,6 +9,7 @@ input = sys.argv[1]
 output = sys.argv[2]
 buffer = int(sys.argv[3])
 indir = os.path.dirname(input)
+topdir = os.path.dirname(os.path.dirname(indir))
 infile = os.path.basename(input)
 
 group, ypos, xpos, size = os.path.splitext(infile)[0].split('_')
@@ -26,8 +27,14 @@ inputs = [input]
 for y in (ypos-size, ypos, ypos+size):
     for x in (xpos-size, xpos, xpos+size):
         if x == xpos and y == ypos: continue
-        path = os.path.join(indir, '*_' + str(y) + '_' + str(x) + '_' + str(size) + '.laz')
+        path = os.path.join(topdir, '*', '*', '*_' + str(y) + '_' + str(x) + '_' + str(size) + '.laz')
         result = glob.glob(path)
+        if len(result) > 1:
+            print("Error: Too many tiles matching glob pattern", path)
+            print(result)
+            exit(1)
+        if len(result) == 0:
+            print("Warning: Missing tile with glob pattern", path)
         if len(result) > 0:
             inputlist.append(reader_las(result[0]))
             inputs.append(result[0])
@@ -47,6 +54,18 @@ pdal_crop = subprocess.Popen(['pdal', 'pipeline', '--stdin', '--stream'], stdin=
 pdal_crop.communicate(input=json.dumps(pipeline_crop).encode('utf-8'))
 assert pdal_crop.returncode == 0
 
+print('Rasterizing small...', output)
+
+pipeline_raster = {'pipeline': []}
+pipeline_raster['pipeline'].append(output + '.laz')
+pipeline_raster['pipeline'].append({ 'type': 'filters.delaunay' })
+pipeline_raster['pipeline'].append({ 'type': 'filters.faceraster', 'resolution': 25.0, 'origin_x': xpos*100, 'origin_y': ypos*100, 'width': size*4, 'height': size*4 })
+pipeline_raster['pipeline'].append({ 'type': 'writers.raster', 'filename': output + '.small.tif', 'gdalopts': 'NUM_THREADS=ALL_CPUS,TILED=YES,COMPRESS=ZSTD' })
+
+pdal_raster = subprocess.Popen(['pdal', 'pipeline', '--stdin'], stdin=subprocess.PIPE)
+pdal_raster.communicate(input=json.dumps(pipeline_raster).encode('utf-8'))
+
+assert pdal_raster.returncode == 0
 
 print('Rasterizing...', output)
 
@@ -62,4 +81,4 @@ pdal_raster.communicate(input=json.dumps(pipeline_raster).encode('utf-8'))
 assert pdal_raster.returncode == 0
 
 # remove filtered point cloud
-#os.remove(output + '.laz')
+os.remove(output + '.laz')
