40d585e2dbfa8b705db75e4ff0113c88400c9499
daniel-j <daniel.j.hede@gmail.com>
2022-06-26 23:00:53 +0200

    update

--- Makefile
+++ Makefile
@@ -3,8 +3,10 @@ INPUTDIR = input
 OUTPUTDIR = output
 LIDARFILES = $(shell find $(INPUTDIR) -maxdepth 3 -type f -name '*.laz' | sort)
 DEMFILES = $(patsubst $(INPUTDIR)/%.laz,$(OUTPUTDIR)/%.tif,$(LIDARFILES))
+OUTDIRS = $(shell find $(OUTPUTDIR) -maxdepth 2 -mindepth 2 -type d | sort)
+VRTFILES = $(patsubst $(OUTPUTDIR)/%,$(OUTPUTDIR)/%.vrt,$(OUTDIRS))
 
-.PHONY: server mount unmount
+.PHONY: server mount unmount dems cog
 .PRECIOUS: $(OUTPUTDIR)/%.tif
 default: $(DEMFILES) dem.vrt
 
@@ -12,7 +14,7 @@ $(OUTPUTDIR)/%.tif: $(INPUTDIR)/%.laz
 	@echo "Processing $<"
 	@mkdir -pv $(dir $@)
 	@time ./process.py "$<" "$@" 500
-	@rio cogeo create "$@.tmp.tif" "$@"
+	@rio cogeo create --use-cog-driver --overview-level 8 "$@.tmp.tif" "$@"
 	@rm -fv "$@.tmp.tif" "$@.laz"
 #	@IFS='_' read -r -a arr <<< "$<"; time pdal pipeline ./pipeline_raster.json --readers.las.filename="$<" --writers.raster.filename="$@.tmp" --filters.faceraster.origin_y="$$(($${arr[1]}*100))" --filters.faceraster.origin_x="$$(($${arr[2]}*100))"
 #	@gdal_fillnodata.py -md 2500 "$@.tmp" "$@" -co COMPRESS=ZSTD
@@ -21,8 +23,12 @@ $(OUTPUTDIR)/%.tif: $(INPUTDIR)/%.laz
 #	@rm -f "$@.tmp"
 	@echo "Finished writing $@"
 
-dem.vrt: $(DEMFILES)
-	@rm -f dem.vrt && gdalbuildvrt -overwrite dem.vrt $(OUTPUTDIR)/*/*/*25.tif
+$(OUTPUTDIR)/%.vrt: $(OUTPUTDIR)/%/*25.tif
+	@echo Generating "$@"
+	@rm -f "$@" && gdalbuildvrt -overwrite "$@" $^
+
+dem.vrt: $(VRTFILES)
+	@rm -f dem.vrt && gdalbuildvrt -overwrite dem.vrt $^
 
 server:
 	uvicorn tiler:app --reload --host 0.0.0.0 --port 3610
--- tiler.py
+++ tiler.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 import rasterio as rio
 from rio_tiler.io import COGReader
@@ -30,7 +31,7 @@ from typing import Any, Dict, List, Optional
 from types import DynamicClassAttribute
 
 
-#import uvicorn
+import uvicorn
 from fastapi import FastAPI, Path, Query, HTTPException
 #from rasterio.crs import CRS
 from starlette.requests import Request
@@ -39,13 +40,15 @@ from starlette.responses import Response
 #from rio_tiler.profiles import img_profiles
 from rio_tiler.io import COGReader
 from rasterio.io import MemoryFile
+from rio_tiler.utils import mapzen_elevation_rgb
+import json
+import numpy
 
-
-cog = COGReader("dem.vrt", maxzoom=17, resampling_method="nearest")
+cog = COGReader("dem.vrt", maxzoom=18, minzoom=11, resampling_method="nearest")
 
 coginfo = cog.info()
 print(coginfo)
-print({'minX': coginfo.bounds.left, 'minZ': coginfo.bounds.bottom, 'maxX': coginfo.bounds.right, 'maxZ': coginfo.bounds.top})
+print(json.dumps({"bounds": {"minX": coginfo.bounds.left, "minZ": coginfo.bounds.bottom, "maxX": coginfo.bounds.right, "maxZ": coginfo.bounds.top}}, indent=4))
 
 app = FastAPI(
     title="rio-tiler",
@@ -98,6 +101,46 @@ async def tile(
             dst.nodata = cog.nodata
         return Response(memfile.read(), media_type="image/tiff")
 
+@app.get(
+    r"/{z}/{x}/{y}.png",
+    responses={
+        200: {
+            "content": {"image/png": {}}, "description": "Return an image.",
+        }
+    },
+    #response_class=ImageResponse,
+    description="Read COG and return a tile",
+)
+async def tilepng(
+    z: int,
+    x: int,
+    y: int
+):
+    """Handle tile requests."""
+    if z < cog.minzoom or z > cog.maxzoom:
+        raise HTTPException(status_code=404, detail="Zoom level out of bounds.")
+    if not cog.tile_exists(x, y, z):
+        raise HTTPException(status_code=404, detail="Tile does not exist")
+    img = cog.tile(x, y, z)
+    count, height, width = img.data.shape
+    output_profile = dict(
+        driver="PNG",
+        dtype='uint8',
+        count=3,
+        height=height,
+        width=width,
+        crs=img.crs,
+        transform=img.transform
+    )
+
+    rgbdata = mapzen_elevation_rgb(img.data[0])
+
+    with MemoryFile() as memfile:
+        with memfile.open(**output_profile) as dst:
+            dst.write(rgbdata)
+        return Response(memfile.read(), media_type="image/png")
+
+
 
 @app.get("/tilejson.json", responses={200: {"description": "Return a tilejson"}})
 async def tilejson(
@@ -110,3 +153,6 @@ async def tilejson(
         "minzoom": cog.minzoom,
         "maxzoom": cog.maxzoom
     }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3610, log_level="info")
